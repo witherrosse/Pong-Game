9b2fd0a4067d9f8fbe37cc0c15075a9e195e3602
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 10:49:55 +0330

    Update Ball.py

--- Pong-Game/Ball.py
+++ Pong-Game/Ball.py
@@ -9,37 +9,42 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        self.x_move = 10    ### Speed in x direction ###
-        self.y_move = 10    ### Speed in y direction ###
+        self.x_move = 10    
+        self.y_move = 10    
         self.move_speed = 0.1   ### Delay for game speed ###
 
-    ### Move the ball by adding speed to position ###
+    
 
     def move(self):
+
+        ''' Move the ball by adding speed to position '''
 
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    ### Reverse vertical direction when hitting top or bottom ###
+    
 
     def bounce_y(self):
 
+        ''' Reverse vertical direction when hitting top or bottom '''
+
         self.y_move *= -1
 
-    ### Reverse horizontal direction when hitting paddle ##
-
-    ### Make the ball faster each time ###
-
+    
     def bounce_x(self):
+
+        ''' Reverse horizontal direction when hitting paddle and
+            Make the ball faster each time '''
 
         self.x_move *= -1
         self.move_speed *= 0.9
 
-    ### Reset ball to center after a player scores ###
+   
 
     def reset_position(self):
+        ''' Reset ball to center after a player scores '''
 
         self.goto(0, 0)
         self.move_speed = 0.1
-        self.bounce_x()   ### Send ball toward the other player ###
+        self.bounce_x()   
