52ba7a02779372b1fb7ba73ff7bcc64d6c2a04c7
Ali Fooladi <alifooladiwither@gmail.com>
2026-07-20 10:54:35 +0330

    Update Scorboardpong.py

--- Pong-Game/Scorboardpong.py
+++ Pong-Game/Scorboardpong.py
@@ -1,8 +1,10 @@
 from turtle import Turtle
 
-### Class to track and display scores for both players ###
+
 
 class Scoreboard(Turtle):
+
+    ''' track and display scores for both players '''
 
     def __init__(self):
 
@@ -10,30 +12,36 @@ class Scoreboard(Turtle):
         self.color("white")
         self.penup()
         self.hideturtle()
-        self.l_score = 0    # Left player score
-        self.r_score = 0    # Right player score
+        self.l_score = 0    
+        self.r_score = 0   
         self.update_score()
 
-    ### Show both scores on screen ###
+    
 
     def update_score(self):
 
-        self.goto(-100, 200)   # Left player score position
+        ''' Show both scores on screen '''
+
+        self.goto(-100, 200)   
         self.write(self.l_score, align="center", font=("courier", 60, "bold"))
-        self.goto(100, 200)    # Right player score position
+        self.goto(100, 200)    
         self.write(self.r_score, align="center", font=("courier", 60, "bold"))
 
-    ### Increase left player score by 1 ###
+    
 
     def l_point(self):
+
+        ''' Increase left player score '''
 
         self.l_score += 1
         self.clear()
         self.update_score()
 
-    ### Increase right player score by 1 ###
+    
 
     def r_point(self):
+
+        ''' Increase right player score '''
 
         self.r_score += 1
         self.clear()
